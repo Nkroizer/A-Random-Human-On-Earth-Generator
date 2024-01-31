3773981c4703362966cea729b3ddba39e0582db1
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-01-31 14:44:23 +0200

    added auto git

---
+++ src/Single-Human-Generator/run_git_save.bat
@@ -0,0 +1,3 @@
+@echo off
+python "C:\Users\Rent\Documents\Repos\A-Random-Human-On-Earth-Generator-1\src\Single-Human-Generator\auto_git.py"
+pause
---
+++ src/Single-Human-Generator/testOutput.py
@@ -0,0 +1,3 @@
+# A book fell on my head the other day. I only have my shelf to blame though.
+# Don't you hate it when someone answers their own questions? I do.
+# A Mexican magician tells the audience he will disappear on the count of three. He says, 'Uno, dos…” and poof! He disappeared without a tres.
