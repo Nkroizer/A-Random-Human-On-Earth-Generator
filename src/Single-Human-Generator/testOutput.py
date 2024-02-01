63b1d9ee276558bb160aefbb42123443dafad0e2
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-02-01 11:44:11 +0200

    comment from python script

--- src/Single-Human-Generator/testOutput.py
+++ src/Single-Human-Generator/testOutput.py
@@ -2,3 +2,4 @@
 # Don't you hate it when someone answers their own questions? I do.
 # A Mexican magician tells the audience he will disappear on the count of three. He says, 'Uno, dos…” and poof! He disappeared without a tres.
 # Build a man a fire and he'll be warm for a day. Set a man on fire and he'll be warm for the rest of his life.
+# I'm reading a book about anti-gravity. It's impossible to put down.
