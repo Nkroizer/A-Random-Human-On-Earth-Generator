44e35d2e0b4a1713e6dcc52fde5f016ebf6dae6d
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-02-01 11:44:15 +0200

    comment from python script

--- src/Single-Human-Generator/testOutput.py
+++ src/Single-Human-Generator/testOutput.py
@@ -3,3 +3,4 @@
 # A Mexican magician tells the audience he will disappear on the count of three. He says, 'Uno, dos…” and poof! He disappeared without a tres.
 # Build a man a fire and he'll be warm for a day. Set a man on fire and he'll be warm for the rest of his life.
 # I'm reading a book about anti-gravity. It's impossible to put down.
+# I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels.
