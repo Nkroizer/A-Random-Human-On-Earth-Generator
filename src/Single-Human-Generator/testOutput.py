f19ea8cdc7cef4543320e1cc48e7e42c97b5a9d6
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-02-02 11:44:08 +0200

    comment from python script

--- src/Single-Human-Generator/testOutput.py
+++ src/Single-Human-Generator/testOutput.py
@@ -4,3 +4,4 @@
 # Build a man a fire and he'll be warm for a day. Set a man on fire and he'll be warm for the rest of his life.
 # I'm reading a book about anti-gravity. It's impossible to put down.
 # I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels.
+# If a parsley farmer gets sued, can they garnish his wages?
