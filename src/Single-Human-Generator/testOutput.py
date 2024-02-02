044db7b11d00172db3db2bbfaa60a6cae0c871de
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-02-02 11:44:22 +0200

    comment from python script

--- src/Single-Human-Generator/testOutput.py
+++ src/Single-Human-Generator/testOutput.py
@@ -6,3 +6,4 @@
 # I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels.
 # If a parsley farmer gets sued, can they garnish his wages?
 # I know they say that money talks, but all mine says is 'Goodbye.'
+# Maybe if we start telling people their brain is an app, they'll want to use it.
