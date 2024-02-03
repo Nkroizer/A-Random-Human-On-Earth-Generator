f7094003cf7eb25ee78e4cbf59df8d76184b4ffd
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-02-03 11:44:08 +0200

    comment from python script

--- src/Single-Human-Generator/testOutput.py
+++ src/Single-Human-Generator/testOutput.py
@@ -10,3 +10,4 @@
 # My wife just found out I replaced our bed with a trampoline. She hit the ceiling!
 # I put my grandma on speed dial the other day. I call it insta-gram.
 # Two wifi engineers got married. The reception was fantastic.
+# What's the difference between ignorance and apathy? I don't know and I don't care.
