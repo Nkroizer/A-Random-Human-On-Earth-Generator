e2ab16b736b628ceb1276f8f71ecc8644cd4f5ce
Nathan Kroyzer <42532605+Nkroizer@users.noreply.github.com>
2024-04-04 11:44:07 +0300

    Fixed the Interactive phased platform

--- src/Single-Human-Generator/example.py
+++ src/Single-Human-Generator/example.py
@@ -3,5 +3,8 @@
 # print(generate_human())
 import random
 
-print("Daddy, Please tell me the story of world: " + str(random.randint(0, 12)))
-
+world_num = random.randint(0, 12)
+addition = ""
+if world_num == 4:
+    addition += "(angel " + str(random.randint(1, 4)) + ")"
+print("Daddy, Please tell me the story of world: " + str(world_num) + " " + addition)
--- src/Single-Human-Generator/testOutput.py
+++ src/Single-Human-Generator/testOutput.py
@@ -197,3 +197,4 @@
 # A Mexican magician tells the audience he will disappear on the count of three. He says, 'Uno, dos…” and poof! He disappeared without a tres.
 # I just got kicked out of a secret cooking society. I spilled the beans.
 # What's the difference between ignorance and apathy? I don't know and I don't care.
+# Always borrow money from a pessimist. They'll never expect it back.
